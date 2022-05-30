072d9f2086e94305080288be4b73266ddd50dd38
Simon Fraser <simon@simoncodes.dev>
2022-05-29 20:37:42 -0700

    add GET methods to get users and tasks

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ const port = process.env.PORT || 3000
 
 app.use(express.json())
 
+//Create a user with POST method
 app.post('/users', (req, res) => {
     const user = new User(req.body)
 
@@ -20,6 +21,7 @@ app.post('/users', (req, res) => {
     })
 })
 
+//Create a task with POST method
 app.post('/tasks', (req, res) => {
     const task = new Task(req.body)
 
@@ -29,6 +31,51 @@ app.post('/tasks', (req, res) => {
         res.status(400).send(e)
     })
 })
+
+//Fetch multiple users with GET method. All users with an empty object in find() statement
+app.get('/users', (req, res) => {
+    User.find({}).then((users) => {
+        res.send(users)
+    }).catch((e) => {
+        res.status(500).send()
+    })
+})
+
+//Fetch specific user by ID with GET method. 
+app.get('/users/:id', (req, res) => {
+    const _id = req.params.id
+    User.findById(_id).then((user) => {
+        if (!user) {
+            return res.status(404).send()
+        }
+        res.send(user)
+    }).catch((e) => {
+        res.status(500).send()
+    })
+})
+
+//Fetch multiple tasks with GET method.
+app.get('/tasks', (req, res) => {
+    Task.find({}).then((tasks) => {
+        res.send(tasks)
+    }).catch((e) => {
+        res.status(500).send()
+    })
+})
+
+//Fetch a single task by ID with GET method. 
+app.get('/tasks/:id', (req, res) => {
+    const _id = req.params.id
+    Task.findById(_id).then((task) => {
+        if (!task) {
+            return res.status(404).send()
+        }
+        res.send(task)
+    }).catch((e) => {
+        res.status(500).send()
+    })
+})
+
 
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
