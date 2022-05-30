ed5aa73dcc37940fd61b8de456661b35e2148a20
Simon Fraser <simon@simoncodes.dev>
2022-05-30 12:00:44 -0700

    Await-Async playground added

--- playground/promise-chaining2.js
+++ playground/promise-chaining2.js
@@ -1,11 +1,23 @@
 require ('../src/db/mongoose')
 const Task = require('../src/models/task')
 
-Task.findByIdAndRemove('62941f6beb59164881920c74').then((query) => {
-    console.log(query)
-    return Task.countDocuments({completed: false})
-}).then((count) => {
-    console.log(count)
+// Task.findByIdAndDelete('62941f6beb59164881920c74').then((query) => {
+//     console.log(query)
+//     return Task.countDocuments({completed: false})
+// }).then((count) => {
+//     console.log(count)
+// }).catch((e) => {
+//     console.log(e)
+// })
+
+const deleteTaskAndCount = async (id) => {
+    const task = await Task.findByIdAndDelete(id)
+    const count = await Task.countDocuments({ completed: false })
+    return count
+}
+
+deleteTaskAndCount('6295136fc04df35b134d02bd').then((count) => {
+    console.log('Completed is false:', count)
 }).catch((e) => {
-    console.log(e)
+    console.log('error', e)
 })
