1e8cd5590f49be0a124fe6d88f491b9abe815e9a
Simon Fraser <simon@simoncodes.dev>
2022-05-29 20:11:19 -0700

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,35 @@
+const { response } = require('express')
+require('./db/mongoose')
+const User = require('./models/user')
+const Task = require('./models/task')
+
+const express = require('express')
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(express.json())
+
+app.post('/users', (req, res) => {
+    const user = new User(req.body)
+
+    user.save().then(() => {
+        res.send(user)
+    }).catch((e) => {
+        res.status(400).send(e)
+    })
+})
+
+app.post('/tasks', (req, res) => {
+    const task = new Task(req.body)
+
+    task.save().then(() => {
+        res.send(task)
+    }).catch((e) => {
+        res.status(400).send(e)
+    })
+})
+
+app.listen(port, () => {
+    console.log('Server is up on port ' + port)
+})
